8545d1b36fe2bf33fb9129b809b866f137082372
Fabio Bergmann <fabiocberg@gmail.com>
2021-11-02 07:05:41 -0300

    feat: barra de busca

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./App.css";
 import MapPage from "./pages/MapPage";
 
+export const REACT_APP_GOOGLE_API_KEY = "CHAVE_DE_API_AQUI"
+
 function App() {
   return <MapPage />;
 }
--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,41 +1,66 @@
-import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
 import React from "react";
+import { GoogleMap, Marker, LoadScript, StandaloneSearchBox } from "@react-google-maps/api";
+import { REACT_APP_GOOGLE_API_KEY } from "../App";
 import "./MapPage.css";
 
 export interface MapPageProps {}
 
 const MapPage = () => {
-  const { isLoaded } = useJsApiLoader({
-    id: "google-map-script",
-    googleMapsApiKey: "AIzaSyAoyKb-HYZnsvRtabAzmZxOQBq34qptkwo",
-  });
+  // const { isLoaded } = useJsApiLoader({
+  //   id: "google-map-script",
+  //   googleMapsApiKey: REACT_APP_GOOGLE_API_KEY,
+  // });
+
+  const [map, setMap] = React.useState<google.maps.Map>();
+  const [searchBox, setSearchBox] = React.useState<google.maps.places.SearchBox>();
+  const [markers, setMarkers] = React.useState<any[]>([]);
 
   const position = {
     lat: -27.590824,
     lng: -48.551262,
   };
 
+  const onMapLoad = (map: google.maps.Map) => {
+    setMap(map)
+  }
+
+  const onLoad = (ref: google.maps.places.SearchBox) => {
+    setSearchBox(ref)
+  };
+
+  const onPlacesChanged = () => {
+    const places = searchBox!.getPlaces();
+    console.log(places);
+    const place = places![0];
+    const location = {
+      lat: place?.geometry?.location?.lat() || 0,
+      lng: place?.geometry?.location?.lng() || 0,
+    
+    }
+    setMarkers([...markers, location])
+    map?.panTo(location)
+  };
+
   return (
     <div className="map">
-      {isLoaded ? (
+      <LoadScript
+        googleMapsApiKey={REACT_APP_GOOGLE_API_KEY}
+        libraries={['places']}>
         <GoogleMap
+          onLoad={onMapLoad}
           mapContainerStyle={{ width: "100%", height: "100%" }}
           center={position}
           zoom={15}
         >
-          <Marker
-            position={position}
-            options={{
-              label: {
-                text: "Posição Teste",
-                className: "map-marker",
-              },
-            }}
-          />
+          <StandaloneSearchBox onLoad={onLoad} onPlacesChanged={onPlacesChanged}>
+            <input className="address" placeholder="Digite um endereço" />
+          </StandaloneSearchBox>
+          {markers.map((marker, index) => (
+            <Marker key={index} position={marker} />
+          ))}
+          
         </GoogleMap>
-      ) : (
-        <></>
-      )}
+      </LoadScript>
     </div>
   );
 };
