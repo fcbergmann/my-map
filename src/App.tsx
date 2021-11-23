b97e265bdd082993f04c80d076b7fcbd578b20b4
Fabio Bergmann <fabiocberg@gmail.com>
2021-11-23 13:04:21 -0300

    feat: traçar rota

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import MapPage from "./pages/MapPage";
 
-export const REACT_APP_GOOGLE_API_KEY = "AIzaSyBRsWlz7lXlwckDBhHID7_D_NniadNdkow"
+export const REACT_APP_GOOGLE_API_KEY = "CHAVE-API";
 
 function App() {
   return <MapPage />;
